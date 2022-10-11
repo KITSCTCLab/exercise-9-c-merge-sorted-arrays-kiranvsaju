e82e2f029ca1f81d6a526dc290781f5d8981a572
kiranvsaju <91770235+kiranvsaju@users.noreply.github.com>
2022-10-11 13:34:19 +0530

    commit

--- Main.py
+++ Main.py
@@ -2,7 +2,25 @@ from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
   # Write code here
-
+  l1 = nums1[:]
+  l2 = nums2[:]
+  curr, i, j = 0, 0, 0
+  while i < m and j < n:
+    if l1[i] < l2[j]:
+      nums1[curr] = l1[i]
+      i += 1
+    else:
+      nums1[curr] = l2[j]
+      j += 1
+    curr += 1
+  while i < m:
+    nums1[curr] = l1[i]
+    curr += 1
+    i += 1
+  while j < n:
+    nums1[curr] = l2[j]
+    curr += 1
+    j += 1
 
 # Do not change the following code
 nums1 = []
